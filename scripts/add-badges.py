d7df727a2cb2918f79094d226d3817f5b7f76e01
Bogdan Popa <popa.bogdanp@gmail.com>
2018-12-03 18:11:33 +0200

    feat: add script to automate badge adding

---
+++ scripts/add-badges.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import re
+import os
+
+README_PATH = os.path.join(os.path.dirname(__file__), "..", "README.md")
+REPO_RE = re.compile(r"https://github.com/(?P<user>[^/]+?)/(?P<repo>[^/\)]+?)\)")
+
+lines = []
+past_solutions = False
+with open(README_PATH) as f:
+    for line in f:
+        if not past_solutions:
+            if "### Solutions" in line:
+                past_solutions = True
+
+            lines.append(line)
+            continue
+
+        match = REPO_RE.search(line)
+        if match is not None:
+            user = match.groupdict()["user"]
+            repo = match.groupdict()["repo"]
+            lines.append(f"* ![Last Commit on GitHub](https://img.shields.io/github/last-commit/{user}/{repo}.svg) [{user}/{repo}](https://github.com/{user}/{repo})\n")
+
+        else:
+            lines.append(line)
+
+with open(README_PATH, "w") as f:
+    f.write("".join(lines))
