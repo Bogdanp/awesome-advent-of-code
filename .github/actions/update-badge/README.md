3b4083a06542ce6d79242418319abf3d3a556988
Xianming Zhong <chinesedfan@qq.com>
2020-06-06 21:53:56 +0800

    init action

---
+++ .github/actions/update-badge/README.md
@@ -0,0 +1,13 @@
+# Update badge action
+
+This action fetches last commit time of repositories and generates the badge for advent-of-code.
+
+## Inputs
+
+### `filePath`
+
+**Required** File path of README.md
+
+## Outputs
+
+No output. The input file will be changed directly. You can use another action to commit.
---
+++ .github/actions/update-badge/index.js
@@ -0,0 +1,41 @@
+const fs = require('fs')
+const core = require('@actions/core')
+
+try {
+  const filePath = core.getInput('filePath')
+  const content = fs.readFileSync(filePath)
+  const repos = extractRepositories(content)
+
+  console.log('extract js repos:')
+  console.log(repos.join('\n'));
+} catch (error) {
+  core.setFailed(error.message);
+}
+
+function extractRepositories(content) {
+  const repos = []
+  const lines = content.split('\n')
+
+  let collect = false
+  lines.some((line) => {
+    if (line === '*Solutions to AoC in JavaScript.*') {
+      collect = true
+    } else if (collect) {
+      if (line.indexOf('####') === 0) return true
+
+      const idx1 = line.indexOf('[')
+      const idx2 = line.indexOf(']')
+      if (idx1 >= 0 && idx2 >= 0) {
+        repos.push(line.slice(idx1 + 1, idx2))
+      }
+    }
+
+    return false
+  })
+
+  return repos
+}
+function fetchLastCommitTime(repo) {
+}
+function generateBadget() {
+}
