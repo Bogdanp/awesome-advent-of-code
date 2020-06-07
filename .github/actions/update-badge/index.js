b4104bbd8b8f207f10267155106b71a14382c22e
Xianming Zhong <chinesedfan@qq.com>
2020-06-07 12:37:02 +0800

    prepare all logic

--- .github/actions/update-badge/index.js
+++ .github/actions/update-badge/index.js
@@ -1,16 +1,21 @@
 const fs = require('fs')
+const axios = require('axios')
 const core = require('@actions/core')
 
-try {
-  const filePath = core.getInput('filePath')
-  const content = fs.readFileSync(filePath, 'utf8')
+(async function () {
+  const inputFile = core.getInput('inputFile')
+  const outputFile = core.getInput('outputFile')
+
+  const content = fs.readFileSync(inputFile, 'utf8')
   const repos = extractRepositories(content)
 
-  console.log('extract js repos:')
-  console.log(repos.join('\n'));
-} catch (error) {
-  core.setFailed(error.message);
-}
+  const lines = await Promise.all(repos.map(generateLine))
+
+  fs.writeFileSync(outputFile, lines.join('\n'))
+  console.log(`finished writing to ${outputFile}`)
+})().catch(error => {
+  core.setFailed(error.message)
+})
 
 function extractRepositories(content) {
   const repos = []
@@ -35,7 +40,24 @@ function extractRepositories(content) {
 
   return repos
 }
-function fetchLastCommitTime(repo) {
+
+async function generateLine(repoStr) {
+  const badge = await generateBadget(repoStr)
+  return `* [${repoStr}](https://github.com/${repoStr}) ![Last Commit on GitHub](${badge})`
 }
-function generateBadget() {
+async function generateBadget(repoStr) {
+  const endpoint = 'https://runkit.io/chinesedfan/last-commit-badge/1.3.0'
+  const [user, repo] = repoStr.split('/')
+  const { label, message, color } = await axios(endpoint, {
+    params: {
+      user,
+      repo
+    }
+  })
+
+  console.log(`...fetched repo ${repoStr}`)
+
+  return 'https://img.shields.io/badge/' + [label, message, color]
+    .map(s => encodeURIComponent(s.replace(/\-/g, '--')))
+    .join('-')
 }
